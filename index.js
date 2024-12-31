4d0c3226057c20a30e3b3d5082300cc7e680b26e
Wissididom <30803034+Wissididom@users.noreply.github.com>
2024-12-31 18:06:16 +0100

    Lint and Format (#7)

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import "dotenv/config";
-import { ActivityType, Client, Events, GatewayIntentBits, Partials } from "discord.js";
+import {
+  ActivityType,
+  Client,
+  Events,
+  GatewayIntentBits,
+  Partials,
+} from "discord.js";
 import { readFileSync } from "node:fs";
 
 const client = new Client({
@@ -7,35 +13,36 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.DirectMessages,
-    GatewayIntentBits.MessageContent
+    GatewayIntentBits.MessageContent,
   ],
   partials: [
     Partials.User,
     Partials.Channel,
     Partials.GuildMember,
     Partials.Message,
-    Partials.Reaction
-  ]
+    Partials.Reaction,
+  ],
 });
 
-const FORBIDDEN_WORDS = JSON.parse(readFileSync('./blocked.json'));
+const FORBIDDEN_WORDS = JSON.parse(readFileSync("./blocked.json"));
 
 client.on(Events.ClientReady, () => {
   console.log("I'm online!");
-  client.user.setActivity('no avoiding the automod', {
-    type: ActivityType.Playing
+  client.user.setActivity("no avoiding the automod", {
+    type: ActivityType.Playing,
   });
 });
 
 client.on(Events.MessageCreate, async (message) => {
   for (const [, attachment] of message.attachments) {
     if (attachment.contentType.startsWith("text/plain")) {
-      let content = await (await fetch(attachment.url)).text();
+      const content = await (await fetch(attachment.url)).text();
       for (const forbiddenWord of FORBIDDEN_WORDS) {
         if (content.includes(forbiddenWord)) {
           await message.delete();
           await message.channel.send({
-            content: `Dear ${message.author.tag}\nPlease do not share such words`
+            content:
+              `Dear ${message.author.tag}\nPlease do not share such words`,
           });
         }
       }
@@ -43,4 +50,4 @@ client.on(Events.MessageCreate, async (message) => {
   }
 });
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
